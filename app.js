a5aa7854a70fa0439ffa782c7cec4f02f8bfef91
anuragpardeshii <158136308+anuragpardeshii@users.noreply.github.com>
2024-06-16 00:28:21 +0530

    commit by anurag

---
+++ app.js
@@ -0,0 +1,28 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
+      console.log("game started");
+      started = true;
+      
+      levelUp();
+    }
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash")
+}
+
+function levelUp(){
+    level++;
+    h2.innerText = `level ${level}`;
+
+    //random button flash
+    btnFlash();
+}
